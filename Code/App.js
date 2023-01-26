b9ee13e15a01bd2a0a971971f4edbffe8fd38b8f
shivamsharmanps <shivamssh@users.noreply.github.com>
2023-01-27 02:50:34 +0530

    Chapter-3
    - Initial

--- Code/App.js
+++ Code/App.js
@@ -8,15 +8,15 @@ const h1 = React.createElement(
                     className: 'h1-header',
                     key:'h1'
           },
-          'Day-2'
+          'Day - 3'
 );
 
 const h2 = React.createElement('h2',
           {
-                    id:'Day2',
+                    id:'Day3',
                     key:'h2'
           },
-          '(Day-2)'
+          '(Day - 3)'
 );
 
 const container = React.createElement('main',
